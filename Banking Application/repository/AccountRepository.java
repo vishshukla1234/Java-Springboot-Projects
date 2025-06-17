d783334ac5f17d6b90fb3875b8b4e18ca373415c
vishshukla1234 <168364704+vishshukla1234@users.noreply.github.com>
2025-06-17 13:33:41 +0530

    Delete Banking Application directory

--- Banking Application/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package net.bank.banking_app.repository;
-
-import net.bank.banking_app.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-}
