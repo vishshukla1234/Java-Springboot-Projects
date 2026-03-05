d4a0d2f93d87de642e45152db146ee2665198736
Vishal Shukla <168364704+vishshukla1234@users.noreply.github.com>
2026-03-05 14:01:16 +0530

    Update Navbar.jsx

--- FileCompresseor/src/Navbar/Navbar.jsx
+++ FileCompresseor/src/Navbar/Navbar.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
+import './Navbar.css'
 
 export default function Navbar() {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
-                <a className="navbar-brand" href="#">FREE File Encrypter</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-            </nav>
+        <div className='navbar'>
+            <div className="text">
+                <h3>File Encryptor</h3>
+            </div>
         </div>
 
     )
