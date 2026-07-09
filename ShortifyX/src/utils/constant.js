ee761498deac0a660c18d3a470305faf321e4448
Vishal Shukla <168364704+vishshukla1234@users.noreply.github.com>
2026-07-09 22:06:57 +0530

    Delete ShortifyX/src directory

--- ShortifyX/src/utils/constant.js
+++
@@ -1,6 +0,0 @@
-import AppRouter, { SubDomainRouter } from "../AppRouter";
-
-export const subDomainList = [
-    {subdomain:"www", app: AppRouter, main: true},
-    {subdomain:"url", app: SubDomainRouter, main: false}
-];
